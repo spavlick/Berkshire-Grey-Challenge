7fc51d001af94f4a50589bd337a98d7bb3a41e53
Stephanie Pavlick <stephaniepavlick100@gmail.com>
2019-04-03 13:06:20 -0400

    Created code with 1 test and updated gitignore

---
+++ coding-challenge.py
@@ -0,0 +1,31 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
+#function to return files with certain word in directory
+def file_search(mypath, regex):
+	regex_files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and regex in f]
+	print("Our code found ", str(len(regex_files)), " files that match your criteria")
+	return regex_files
+
+def test1():
+	mypath = os.path.join(os.getcwd(), 'test1/')
+	if not os.path.isdir(mypath):
+		os.mkdir(mypath)
+	#create files in directory
+	for i in range(3):
+		f = open(mypath+str('hello'+str(i)+'.txt'),'w')
+		f.close()
+		f = open(mypath+str('bye'+str(i)+'.txt'),'w')
+	files = file_search(mypath,'hello')
+	print(files)
+
+
+
+def user_input():
+	mypath = raw_input("Enter directory to search: ") # find directory desired
+	#criteria = raw_input("")
+	regex = raw_input("Enter regex to search: ")
+	file_search(mypath,regex)
+
+test1()
